064bdb0893d84b003cdeab3e41d8e1aeca95b5bb
Enrique <enri.r99@gmail.com>
2020-03-12 14:05:06 +0100

    Arreglando training y añadiendo datos a la BBDD

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -69,15 +69,53 @@ INSERT INTO trainings_routines(training_id,routines_id) VALUES (4,1);
 
 /* EQUIPMENT */
 
-INSERT INTO equipments(id,name,location) VALUES (1,'Gemelos_10000','Musculación');
-INSERT INTO equipments(id,name,location) VALUES (2,'Biceps_9000','Musculación');
-INSERT INTO equipments(id,name,location) VALUES (3,'Cinta de Correr','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (1,'Squat-Calf','Muscle and Tonification');
+INSERT INTO equipments(id,name,location) VALUES (2,'Bars 30-50 mm','Muscle and Tonification');
+INSERT INTO equipments(id,name,location) VALUES (3,'Weight-Disc 30-50 mm','Muscle and Tonification');
+INSERT INTO equipments(id,name,location) VALUES (4,'Dumbbells','Muscle and Tonification');
+INSERT INTO equipments(id,name,location) VALUES (5,'Kettlebells','Muscle and Tonification');
+INSERT INTO equipments(id,name,location) VALUES (6,'Treadmill','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (7,'Elliptical','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (8,'Spin-Bike','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (9,'Stair-Mill','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (10,'Rowing Machine','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (11,'Skierg','Cardio');
+INSERT INTO equipments(id,name,location) VALUES (12,'Recumbent-Bike','Cardio');
 
 /* EXERCISE */
 
-INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (1,'Gemelos', 'Arriba abajo', 50, 2, 1);
-INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (2,'Biceps Normales', 'Arriba abajo', 50, 2, 2);
-INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (3,'Correr', '20 minutos v=12', 200, 3, 3);
+/* Equipment 1 */
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (1,'Squat','Shift your weight like if you were crouching', 100, 2, 1);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (2,'Single Leg Squat','Shift your weight like if you were crouching with one leg. Alter legs', 100, 2, 1);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (3,'Calf Raises','Stand on your tiptoes. Support weight with both legs', 100, 2, 1);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (4,'Single Leg Calf Raises','Stand on your tiptoes with one leg. Just support your weight with one log. Alter legs', 100, 2, 1);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (5,'Lunges','Take a step behind with one leg, then shift your weight down to kneel. Alter legs', 100, 2, 1);
+
+/* Equipment 2 */
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (6,'Squat','Shift your weight like if you were crouching', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (7,'Single Leg Squat','Shift your weight like if you were crouching with one leg. Alter legs', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (8,'Calf Raises','Stand on your tiptoes. Support weight with both legs', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (9,'Single Leg Calf Raises','Stand on your tiptoes with one leg. Just support your weight with one log. Alter legs', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (10,'Lunges','Take a step behind with one leg, then shift your weight down to kneel. Alter legs', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (11,'Curtsy Lunges','Normal Lunge but crossing legs', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (12,'Dead Lift','Normal Lunge but crossing legs', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (13,'Bent-Over Row','Bend your back forward. Bend knees a little bit and hold the bar horizontally at your hips. Extend your arms straight down and then up again', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (14,'Single Arm Row','Like Bent-Over Row, but positioning varies. Back bend forward and one knee forward. One arm hold the forward knee, the other hold the bar vertically.  Extend your arm straight down and then up to the hip', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (15,'Biceps Curl','Hold bar horizontally with both hands. Bend your arms to your chest', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (16,'Shoulder Press','Knees a bit bend and your a bit separated. Hold bar horizontally up your chest. Extends your arms right above your head', 100, 2, 2);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (17,'Chest Press','Lie on the ground. Hold bar horizontally to your chest. Kness bent and feet planted on the floor. Extends your arms straight up and then down', 100, 2, 2);
+
+/* Equipment 3 */
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (18,'Plank Disc Switch','Start by doing a plank. Place one disc next to one arm. Then move the disc next to the grabbing arm. Now move the disc to the starting place an repeat',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (19,'Disc Push Up','Make a Push up having under your hands discs. The more disc the difficultier',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (20,'Front Shoulder Raise','Hold the disc with both hands with your arms straight forward and the disc at chest height. Like a pendulum raise down the disc until your reach your heap. Then go up and repeat',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (21,'Lateral Raise','Grab with each hand a disc. Start with them at your hips, then rise up your arms until they are horizontal to the floor. Return to starting position and repeat',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (22,'Halo','Hold one disc with both hands with your arms slightly bend in fron of you at head height. Move the disc all around your head and return to starting position, then repeat',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (23,'Squat Press','Start at a crouching position holding a disc with both hands and support your hands with your knees. Stand up and hold the disc above your head. Return to starting position and repeat',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (24,'Squat Reach','Start at a crouching position holding a disc with both hands having you arms straighten and the disc at chest height. Stand up and bend arms until the disc touches your chest. Return to starting position and repeat',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (25,'Side Bend','Hold a disc with one hand, the other at your hip. Slightly bend your upper trunk towards the disc side and then straighten up. Repeat and alternate the arm supporting the disc',100,2,3);
+INSERT INTO exercises(id,name,description,kcal,intensity,equipment_id) VALUES (26,'Overhead Press','Hold a disc with both hands and horizontal to the floor. Disc should be at neck height, then move it above your head with extended arms. Return to starting position and repeat',100,2,3);
+
 
 /* ROUTINE LINE */
 
