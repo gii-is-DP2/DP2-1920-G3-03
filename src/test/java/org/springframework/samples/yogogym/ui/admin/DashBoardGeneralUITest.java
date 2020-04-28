5f3ad5bc5ba65e739e63a6d0a6f20dace51251c5
Fran Quintela <fraquivel@alum.us.es>
2020-04-28 13:12:25 +0200

    Random port update

--- src/test/java/org/springframework/samples/yogogym/ui/admin/DashBoardGeneralUITest.java
+++ src/test/java/org/springframework/samples/yogogym/ui/admin/DashBoardGeneralUITest.java
@@ -3,13 +3,24 @@ package org.springframework.samples.yogogym.ui.admin;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DashBoardGeneralUITest {
+  
+  @LocalServerPort
+  private int port;
+  
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -24,22 +35,8 @@ public class DashBoardGeneralUITest {
 
   @Test
   public void testDashBoardClientUI() throws Exception {
-    driver.get("http://localhost:8080/");
-    driver.findElement(By.linkText("Login")).click();
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("admin1999");
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("admin1");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.linkText("Admin")).click();
-    driver.findElement(By.xpath("//div[@id='bs-example-navbar-collapse-1']/ul/li[2]/ul/li[5]/a/span[2]")).click();
-    assertEquals("Clients:", driver.findElement(By.xpath("//h3")).getText());
-    assertEquals("Trainers:", driver.findElement(By.xpath("//h3[2]")).getText());
-    driver.findElement(By.id("canvasClientsPerGuilds")).click();
-    driver.findElement(By.id("canvasClientsPerGuilds")).click();
-    driver.findElement(By.linkText("admin1")).click();
-    driver.findElement(By.linkText("Logout")).click();
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		as("admin1");
+    showDashboardGeneral();
   }
 
   @AfterEach
@@ -51,38 +48,23 @@ public class DashBoardGeneralUITest {
     }
   }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
+  private void as(String username) {
+		driver.get("http://localhost:" + port);
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("admin1999");
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
   }
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
-  
+  private void showDashboardGeneral() {
+    driver.findElement(By.linkText("Admin")).click();
+    driver.findElement(By.xpath("//div[@id='bs-example-navbar-collapse-1']/ul/li[2]/ul/li[5]/a/span[2]")).click();
+    assertEquals("Clients:", driver.findElement(By.xpath("//h3")).getText());
+    assertEquals("Trainers:", driver.findElement(By.xpath("//h3[2]")).getText());
+    driver.findElement(By.id("canvasClientsPerGuilds")).click();
+    driver.findElement(By.id("canvasClientsPerGuilds")).click();
+	}
 }
 
