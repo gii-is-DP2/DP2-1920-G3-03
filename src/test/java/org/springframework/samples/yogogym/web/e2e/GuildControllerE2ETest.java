79ae8cc707b2bb88adbd0fff0d97f059343e7e7c
alvagualh <alvagualh@alum.us.es>
2020-05-05 23:52:58 +0200

    Otro intento

--- src/test/java/org/springframework/samples/yogogym/web/e2e/GuildControllerE2ETest.java
+++ src/test/java/org/springframework/samples/yogogym/web/e2e/GuildControllerE2ETest.java
@@ -144,7 +144,6 @@ public class GuildControllerE2ETest {
 				.andExpect(model().attributeExists("guild"));
 	}
 	
-	/*
 	@WithMockUser(username = "client1", authorities = { "client" })
 	@Test
 	void testProcessUpdateGuildForm() throws Exception {
@@ -160,7 +159,7 @@ public class GuildControllerE2ETest {
 				.param("name",guild.getName()))
 				.andExpect(status().is3xxRedirection())
 				.andExpect(view().name("redirect:/client/" + clientUsername + "/guilds/" + guildId));
-	}*/
+	}
 	
 	@WithMockUser(username = "client1", authorities = { "client" })
 	@Test
