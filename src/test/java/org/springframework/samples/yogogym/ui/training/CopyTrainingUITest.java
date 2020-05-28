9d15ca125979180e8c780845028758df4fe5a02d
alvagualh <alvagualh@alum.us.es>
2020-05-29 01:38:58 +0200

    Fix CopyTrainingUI

--- src/test/java/org/springframework/samples/yogogym/ui/training/CopyTrainingUITest.java
+++ src/test/java/org/springframework/samples/yogogym/ui/training/CopyTrainingUITest.java
@@ -3,6 +3,7 @@ package org.springframework.samples.yogogym.ui.training;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterEach;
@@ -14,67 +15,66 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CopyTrainingListTrainingNotEmptyUITest {
+public class CopyTrainingUITest {
 
 	@LocalServerPort
 	private int port;
 
 	private WebDriver driver;
-	private StringBuffer verificationErrors = new StringBuffer();
+	UtilsTrainingUI utils;
+
+	// Globally used
+	private static final String TRAINER1_USERNAME = "trainer1";
+	private static final String TRAINER1_PASS = "trainer1999";
+	private static final String NEW_TRAINING_NAME = "Nuevo Entrenamiento";
 
 	@BeforeEach
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
-	@Test
-	public void testCopyTrainingUI() throws Exception {
-		as("trainer1");
-		listTrainingToCopy();
-		exceptionViewShown();
+		utils = new UtilsTrainingUI(port, driver);
 	}
 
 	@AfterEach
 	public void tearDown() throws Exception {
 		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
+		String verificationErrorString = utils.getVerificationError().toString();
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
 		}
 	}
 
-	private void as(String username) {
-		driver.get("http://localhost:" + port);
-		driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys("trainer1999");
-		driver.findElement(By.id("username")).clear();
-		driver.findElement(By.id("username")).sendKeys(username);
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
+	@Test
+	public void testCopyTrainingUISuccessful() throws Exception {
+		utils.as(TRAINER1_USERNAME, TRAINER1_PASS);
+
+		Calendar calInit = Calendar.getInstance();
+		Calendar calEnd = Calendar.getInstance();
+		calEnd.add(Calendar.DAY_OF_MONTH, 7);
+		utils.createTraining(NEW_TRAINING_NAME, calInit, calEnd, false);
+
+		utils.copyTraining(NEW_TRAINING_NAME);
 		try {
-			assertEquals(username, driver
-					.findElement(By.xpath("//div[@id='bs-example-navbar-collapse-1']/ul[2]/li/a/strong")).getText());
+			assertEquals("Cardio", driver.findElement(By.linkText("Cardio")).getText());
 		} catch (Error e) {
-			verificationErrors.append(e.toString());
+			utils.getVerificationError().append(e.toString());
 		}
 	}
 
-	private void listTrainingToCopy() {
+	@Test
+	public void testCopyTrainingUIWrong() throws Exception {
+		utils.as(TRAINER1_USERNAME, TRAINER1_PASS);
+
 		driver.get("http://localhost:" + port + "/trainer/trainer1/clients/1/trainings/1/copyTraining");
-	}
-	
-	private void exceptionViewShown() {
-		try {
-			assertEquals("Something happened...", driver.findElement(By.xpath("//h2")).getText());
-		} catch (Error e) {
-			verificationErrors.append(e.toString());
-		}
-	}
 
+		utils.exceptionView();
+	}
 
 }
